2afc9ab3f5a9c3746f0d8f87935095d4adb61779
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-04-02 16:28:25 -0500

    ejs added

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.use('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+
+  var today = new Date();
+
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.sendFile(__dirname + "/weekend.html");
+  } else {
+    res.sendFile(__dirname + "/weekday.html");
+  }
+
 });
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
   console.log("Success!");
 });
