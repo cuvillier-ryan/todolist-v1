9fce9ced8816033b0cfac5e02843ba34f1e5a76f
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-04-03 18:52:52 -0500

    added switch statement

--- app.js
+++ app.js
@@ -3,21 +3,47 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
 
   var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.sendFile(__dirname + "/weekend.html");
-  } else {
-    res.sendFile(__dirname + "/weekday.html");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Mondayy";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Satuday";
+      break;
+
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+
   }
 
+  res.render("list", {
+    day: day
+  });
+
 });
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
